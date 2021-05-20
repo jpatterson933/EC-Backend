60f107bd13135b5149937fd126b23b143c6f8cc0
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-20 15:49:59 -0700

    Added route to grab product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,7 +20,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id);
+    if (!productData) {
+      res.status(404).json({ message: 'Product does not exist' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
+
+
+
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
