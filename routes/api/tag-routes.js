81d54241f20d589756732afb3647c5b82e3c2a92
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-20 16:20:59 -0700

    Added grab all route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,12 +3,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: Product
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id);
+    if (!tagData) {
+      res.status(404).json({ message: 'Tag does not exist' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
