904434ac549c8397d30b602476ce64da11069845
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-20 15:52:32 -0700

    Added route to create product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,15 +32,20 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 
-
-
-
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+  
   /* req.body should look like this...
     {
       product_name: "Basketball",
