b55926a20d62cc5fae4f3a2f4a760811840cd543
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-20 16:27:48 -0700

    Added route to create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -31,7 +31,13 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new tag
 });
 
