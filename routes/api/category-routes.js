fc976a9aa269bccbf40d5611cda8cafca2aae9e4
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-20 11:09:32 -0700

    Added route that updates categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,21 @@ router.post('/', async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No Category to update!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // update a category by its `id` value
 });
 
