61f5f0f14ff10609c7da8522cb810422ed2dc824
Jeffery Patterson <jpatterson933@ucla.edu>
2021-05-20 11:05:06 -0700

    Added rout to grab category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,7 +17,19 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+    if (!categoryData) {
+      res.status(404).json({ message: 'Category does not exist!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // find one category by its `id` value
   // be sure to include its associated Products
 });
